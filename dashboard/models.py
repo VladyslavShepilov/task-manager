292cb4e3308ec4ca60ddb89b47138c49cd9931fc
Vladyslav Shepilov <shepsterya@gmail.com>
2024-02-14 16:37:13 +0200

    add custom user model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Employee(AbstractUser):
+
+    class Role(models.TextChoices):
+        DEV = "dev", "Developer"
+        PM = "pm", "Project Manager"
+        QA = "qa", "QA"
+        DESIGNER = "designer", "Designer"
+        DEVOPS = "devops", "DevOps"
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        blank=False
+    )
+    date_joined = models.DateField(
+        auto_now_add=True
+    )
